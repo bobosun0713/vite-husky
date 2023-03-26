e1b001c2174e36f17182fda224a984d9e11eb90b
bobosun0713 <chiabo.sun@gmail.com>
2023-03-26 22:32:15 +0800

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin } from "vite-plugin-vue2";
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import { createVuePlugin } from 'vite-plugin-vue2';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [createVuePlugin()],
-})
+export default defineConfig(() => ({
+  devtools: false,
+  server: {
+    port: 8080,
+  },
+  preview: {
+    port: 8080,
+  },
+  plugins: [
+    createVuePlugin(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+}));

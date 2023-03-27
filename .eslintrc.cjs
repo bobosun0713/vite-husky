d58baf81ddfb957cfa51fa3fc614f443268fd135
Jiabo Sun 孫佳播 <2203350@C0971.local>
2023-03-27 12:16:43 +0800

    chore(eslint): update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,42 @@
-const path = require('node:path');
-const createAliasSetting = require('@vue/eslint-config-airbnb/createAliasSetting');
+const path = require("node:path");
+const createAliasSetting = require("@vue/eslint-config-airbnb/createAliasSetting");
 
 module.exports = {
+  root: true,
   env: {
     browser: true,
-    node: true,
+    node: true
   },
-  extends: ["@vue/airbnb","plugin:import/recommended", "plugin:vue/strongly-recommended", "prettier", "prettier/prettier"],
-  plugins: ['vue', 'simple-import-sort', 'import'],
+  extends: [
+    "@vue/eslint-config-airbnb",
+    "plugin:import/recommended",
+    "plugin:vue/strongly-recommended",
+    "prettier",
+    "prettier/prettier"
+  ],
+  plugins: ["vue", "simple-import-sort", "import"],
   rules: {
-    'vue/multi-word-component-names': 'error',
-    'vue/no-reserved-component-names': 'error',
+    "vue/multi-word-component-names": "off",
+    "vue/no-reserved-component-names": "off",
+    "vue/mustache-interpolation-spacing": ["error", "always"],
+    "vue/prop-name-casing": "error",
+    "vue/require-default-prop": "error",
+    "vue/require-prop-types": "error",
+
+    "no-console": "off",
+    "no-debugger": "off",
+    "no-empty": ["error", { allowEmptyCatch: true }],
+    quotes: ["error", "double"],
+
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error"
   },
   settings: {
     ...createAliasSetting({
-      '@': `${path.resolve(__dirname, './src')}`,
-    }),
-  },
+      "@": `${path.resolve(__dirname, "./src")}`
+    })
+  }
 };
